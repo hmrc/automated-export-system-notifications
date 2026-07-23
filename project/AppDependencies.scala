9c87049ddf1905aa2e5d39ff45310b4784094caf
tudorsonycx <79589391+tudorsonycx@users.noreply.github.com>
2026-07-23 12:36:25 +0100

    AES-413: upgrade library dependencies and plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,19 +1,18 @@
-import sbt.Keys.libraryDependencies
-import sbt._
+import sbt.*
 
 object AppDependencies {
+  private val bootstrapVersion = "10.8.0"
+  private val playVersion      = "play-30"
 
-  private val bootstrapVersion = "10.7.1"
-  
-
-  val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion
+  val compile: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% s"bootstrap-backend-$playVersion" % bootstrapVersion
   )
 
-  val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
-    
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"            %% s"bootstrap-test-$playVersion" % bootstrapVersion % Test,
+    "org.scalatest"          %% "scalatest"                    % "3.2.20"         % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play"           % "7.0.2"          % Test,
+    "org.scalacheck"         %% "scalacheck"                   % "1.19.0"         % Test,
+    "org.scalatestplus"      %% "scalacheck-1-19"              % "3.2.20.0"       % Test
   )
-
-  val it = Seq.empty
 }
