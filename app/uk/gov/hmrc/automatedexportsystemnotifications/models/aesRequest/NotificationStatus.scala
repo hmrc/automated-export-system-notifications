ecb703183d5fe3d730da8748b60e8297c189ce87
Abigail Houston <abigail.houston1@digital.hmrc.gov.uk>
2026-09-24 13:24:45 +0100

    AES-958: Fixed error with rejections

--- app/uk/gov/hmrc/automatedexportsystemnotifications/models/aesRequest/NotificationStatus.scala
+++ app/uk/gov/hmrc/automatedexportsystemnotifications/models/aesRequest/NotificationStatus.scala
@@ -20,6 +20,6 @@ sealed trait NotificationStatus { def value: Int }
 
 object NotificationStatus {
   case object Accepted extends NotificationStatus { val value = 1 }
-  case object Diversion extends NotificationStatus { val value = 2 }
-  case object Rejected extends NotificationStatus { val value = 5 }
+  case object Diversion extends NotificationStatus { val value = 5 }
+  case object Rejected extends NotificationStatus { val value = 2 }
 }
